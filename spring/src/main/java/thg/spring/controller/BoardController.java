767b2fda30430efe99dcc0c767c95d3b946ce2fd
Caio Santos <caio.cesar@picpay.com>
2022-04-06 18:06:48 -0300

    add front

--- spring/src/main/java/thg/spring/controller/BoardController.java
+++ spring/src/main/java/thg/spring/controller/BoardController.java
@@ -18,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/board")
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BoardController {
 
     private final CreateBoardUseCase createBoardUseCase;
